9f853ad88ec3b445a31b97122a2c17cbe5388e30
dylanocallaghan <dylan.oc.1999@gmail.com>
2022-01-05 04:47:56 +0000

    fav icon update

--- power_zone/settings.py
+++ power_zone/settings.py
@@ -182,7 +182,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'power-zone'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -209,6 +209,17 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51KDH3GIPINVTLFIZuB4knQif93PuA5vX00oFkzhPp1QmzHJvL03bqIk8fakIJeBNATewnGIE41G5BzI0YH4ezbK100q3F4NThg')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51KDH3GIPINVTLFIZyR3DYyXH8Kesi6wNLxIkidQfH6peDIvPMISbv5Wf3coRif9XV0lgFCRg4YoaQyKESGgEUg1c00rDBnC5g2')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_32j3uwH3nLmNDtdLohJ6KxZV1w5DBdMP')
-DEFAULT_FROM_EMAIL = 'powerzone@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'powerzone@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
